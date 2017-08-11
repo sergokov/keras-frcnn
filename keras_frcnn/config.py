a1b71ff7a98814970db0419c9214f22d6a6d9099
sergo <sergokov@gmail.com>
2017-08-11 21:41:02 +0300

    Start training from checkpoint.

--- keras_frcnn/config.py
+++ keras_frcnn/config.py
@@ -56,3 +56,5 @@ class Config:
 		# https://github.com/fchollet/deep-learning-models/releases/download/v0.2/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5
 
 		self.model_path = 'model_frcnn.vgg.hdf5'
+
+		self.training_from_scratch = True
